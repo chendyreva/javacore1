5bab9c8e9c8536bb2554a0dba80883d74e83d74e
chendyreva <nat-217@mail.ru>
2018-07-13 14:26:10 +0500

    запускает клиентскую и серверную части

---
+++ Main.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.chat;
+
+public class Main {
+    public static void main(String[] args) {
+        ClientWindow clientWindow = new ClientWindow();
+        Server server = new Server();
+
+    }
+}
+
+
+
